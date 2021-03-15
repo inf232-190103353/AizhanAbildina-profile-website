be936c322c293473e45c1c0c37d48b6464a2d662
Aizhan <190103353@stu.sdu.edu.kz>
2021-03-15 08:38:47 +0600

    post test

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table= "posts";
-    protected $fillable = ['title'];
+    protected $fillable = ['title' , 'body'];
+
 
 }
